acb4ecdb00951d07b15392b33bcf1e34ff71d807
shashankbhosagi <shashankbhosagi0121@gmail.com>
2024-06-23 15:02:07 +0530

    ✨Add address and gmap pin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div>
       <div className="communicate">
-        <div className="communicate-container">
+        {/* <div className="communicate-container">
           <div
             style={{
               display: "flex",
@@ -23,6 +23,27 @@ const Footer = () => {
               <input type="text" name='message' placeholder='Your Message' required />
               <button type="submit">Send</button>
             </form>
+          </div>
+        </div> */}
+        <div className="contact-container">
+          <div className="left">
+            <iframe
+              id='map'
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.9884541697681!2d73.82800480115826!3d18.635639587356817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9d2682a1951%3A0x6be65fd7643773b7!2sRollndrive!5e0!3m2!1sen!2sin!4v1719129802313!5m2!1sen!2sin"
+              width="1200"
+              height="400"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="right">
+            <h2>RollnDrive</h2>
+            <address>
+            PLOT PAP-128 J BLOCK MIDC,<br/> BEHIND SMITH INTERNATIONAL,<br/> Telco Road, NEAR QUALITY CIRCLE,<br/> Bhosari, Pimpri Chinchwad,<br/> Pune, Maharashtra, 411026.<br/> <b>GST no: 27BHMPC3508H1ZH</b><br/><br/>Mob no: +91 9637899174<br/>Email: <a href="mailto:vishal@rollndrive.com">vishal@rollndrive.com</a>
+
+            </address>
           </div>
         </div>
       </div>
