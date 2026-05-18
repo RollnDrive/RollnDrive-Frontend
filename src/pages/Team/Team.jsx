c526817a597826b448b35fbb54f083c2721ba8a7
Tapan Patil <tapanpatil010@gmail.com>
2026-05-18 10:32:36 -0400

    Reorder MemberCard for Shivashankar Khobare

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -23,8 +23,8 @@ const Team = () => {
         <div className="members">
           
           <MemberCard image={vishal} name={"Vishal Chepuri"} role={"Founder"}></MemberCard>
-          <MemberCard image={shivashankar} name={"Shivashankar Khobare"} role={"Production Manager"}></MemberCard>
           <MemberCard image={tapan} name={"Tapan Patil"} role={"Co-Founder"}></MemberCard>
+          <MemberCard image={shivashankar} name={"Shivashankar Khobare"} role={"Production Manager"}></MemberCard>
           <MemberCard image={sahadev} name={"Sahadev Achwale"} role={"Store Manager"}></MemberCard>
         </div>
       </section>
